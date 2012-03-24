57e2419a8339d5ae1d6bb42a648754fb3b7616b0
Mitchell Cooper <cooper879@msn.com>
2012-03-24 15:59:17 -0400

    0.6: cleaned up a bit. added recaptcha support, the on_gotcaptcha callback, the submit_captcha() method, etc. fixed lag issues by using two-second intervals. added a small start to Omegle's new common interest system. if enabled, alternate through servers provided by Omegle rather than hard-coding them. Omegle's global online user count as ::Omegle::online. cleaned up a bit.

--- New/Omegle.pm
+++ New/Omegle.pm
@@ -4,67 +4,73 @@ package New::Omegle;
 
 use warnings;
 use strict;
-use feature 'switch';
+use 5.010;
 
 use HTTP::Async;
 use HTTP::Request::Common;
-use LWP::UserAgent;
 use JSON;
 
-our $VERSION    = '0.5';
-my  @servers    = qw[bajor cardassia promenade odo-bucket ferengi quarks];
-my  $lastserver = 2;
+our ($VERSION, $online, @servers) = (0.6, 0);
+my  $lastserver;
 
 sub new {
     my ($class, %opts) = @_;
-    bless my $om    = \%opts, $class;
-    $om->{async}    = new HTTP::Async;
-    $om->{ua}       = new LWP::UserAgent;
-    $om->{json}     = new JSON;
-    $om->{typing}   = 0;
-    return $om
+    $opts{async} = HTTP::Async->new;
+    bless my $om = \%opts, $class;
+    return $om->update;
 }
 
 sub start {
     my $om = shift;
-    $om->{server} = 'http://'.newserver() unless $om->{static};
-    my $res = $om->{ua}->post("$$om{server}/start");
-    my $id = $res->content || '';
-    $id =~ s/"//g;
-    return undef unless $id;
-    $om->{id} = $id;
+    $om->{last_time} = time;
+    $om->{async}   ||= new HTTP::Async;
+    $om->{server}    = &newserver unless $om->{static};
+
+    my $startopts = '?rcs=&spid=';
+
+    # get ID
+    $om->{async}->add(POST "http://$$om{server}/start$startopts");
+    while (my $res = $om->{async}->wait_for_next_response) {
+        next unless $res->content =~ m/^"(.+)"$/;
+        $om->{id} = $1;
+        last
+    }
+
+    $om->{stopsearching} = time() + 5 if $om->{use_likes};
     $om->request_next_event;
-    return $id
+    return $om->{id};
 }
 
 sub newserver {
-    if ($lastserver == $#servers) {
-        $lastserver = 0;
-        return $servers[0]
-    }
-    return $servers[++$lastserver].'.omegle.com';
+    $servers[$lastserver == $#servers ? $lastserver = 0 : ++$lastserver];
 }
 
 sub request_next_event {
     my $om = shift;
     return unless $om->{id};
-    $om->{async}->add(POST "$$om{server}/events", [ id => $om->{id} ])
+    $om->post('events');
 }
 
 sub get_next_events {
     my $om = shift;
     my @f = ();
-    while (my $res = $om->{async}->next_response) {
-        push @f, $res
-    }
-    return @f
+    while (my @res = $om->{async}->next_response) { push @f, \@res }
+    return @f;
 }
 
 sub handle_events {
-    my ($om, $json) = @_;
-    return unless $json =~ m/^\[/;
-    my $events = $om->{json}->decode($json);
+    my ($om, $data, $req_id) = @_;
 
+    # captcha response
+    if (defined $om->{pending_captcha} && $req_id == $om->{pending_captcha}) {
+        delete $om->{pending_captcha};
+    }
+
+    # array of events must start with [
+    return unless $data =~ m/^\[/;
+
+    # event JSON
+    my $events = JSON::decode_json($data);
     foreach my $event (@$events) {
         $om->handle_event(@$event);
     }
@@ -82,24 +88,59 @@ sub callback {
 sub handle_event {
     my ($om, @event) = @_;
     given ($event[0]) {
+
+        # session established
         when ('connected') {
             $om->callback('on_connect')
         }
+
+        # stranger said something
         when ('gotMessage') {
             $om->callback('on_chat', $event[1]);
             $om->{typing} = 0
         }
+
+        # stranger disconnected
         when ('strangerDisconnected') {
             $om->callback('on_disconnect');
             delete $om->{id}
         }
+
+        # stranger is typing
         when ('typing') {
             $om->callback('on_type') unless $om->{typing};
             $om->{typing} = 1
         }
+
+        # stranger stopped typing
         when ('stoppedTyping') {
             $om->callback('on_stoptype') if $om->{typing};
             $om->{typing} = 0
+        }
+
+        # stranger has similar interests
+        when ('commonLikes') {
+            $om->callback('on_commonlikes', $event[1]);
+        }
+
+        # number of people online
+        when ('count') {
+            $online = $event[1];
+            $om->callback('on_count', $event[1]);
+        }
+
+        # server requests captcha
+        when (['recaptchaRequired', 'recaptchaRejected']) {
+            my $rand = rand;
+            my $name = $event[1];
+            my $id   = $om->{async}->add(GET "http://google.com/recaptcha/api/challenge?k=$name&ajax=1");
+            while (my ($res, $this_id) = $om->{async}->wait_for_next_response) {
+                next   unless $id == $this_id;
+                return unless $res->content =~ m/challenge : '(.+)'/;
+                $om->{challenge} = $1;
+                $om->callback('on_gotcaptcha', "http://www.google.com/recaptcha/api/image?c=$1");
+                last
+            }
         }
     }
     return 1
@@ -108,37 +149,79 @@ sub handle_event {
 # request and handle events: put this in your main loop
 sub go {
     my $om = shift;
-	foreach my $res ($om->get_next_events) {
-	    next unless $res;
-        $om->handle_events($res->content);
+    return unless $om->{id};
+    return if (($om->{last_time} + 2) > time);
+
+    # stop searching for common likes
+    if (defined $om->{stopsearching} && $om->{stopsearching} >= time) {
+        $om->post('stoplookingforcommonlikes');
+        $om->callback('on_stopsearching');
+        delete $om->{stopsearching};
     }
-    $om->request_next_event
+
+    # look for new events
+	foreach my $res ($om->get_next_events) {
+	    next unless $res->[0];
+        $om->handle_events($res->[0]->content, $res->[1]);
+    }
+
+    $om->request_next_event;
+    $om->{last_time} = time;
+}
+
+# update status
+sub update {
+    my $om = shift;
+    $om->{async}->add(POST "http://omegle.com/status");
+    my $data    = JSON::decode_json($om->{async}->wait_for_next_response->content);
+    @servers    = @{$data->{servers}};
+    $lastserver = $#servers;
+    $online     = $data->{count};
+    return $om;
+}
+
+# submit recaptcha request
+sub submit_captcha {
+    my ($om, $response) = @_;
+    $om->{pending_captcha} = $om->post('recaptcha', [
+        challenge => delete $om->{challenge},
+        response  => $response
+    ]);
 }
 
 # send a message
 sub say {
     my ($om, $msg) = @_;
     return unless $om->{id};
-    $om->{async}->add(POST "$$om{server}/send", [ id => $om->{id}, msg => $msg ])
+    $om->post('send', [ msg => $msg ]);
 }
 
 # make it appear that you are typing
 sub type {
     my $om = shift;
     return unless $om->{id};
-    $om->{async}->add(POST "$$om{server}/typing", [ id => $om->{id} ])
+    $om->post('typing');
 }
 
 # make it appear that you have stopped typing
 sub stoptype {
     my $om = shift;
     return unless $om->{id};
-    $om->{async}->add(POST "$$om{server}/stoptyping", [ id => $om->{id} ])
+    $om->post('stoptyping');
 }
 
 # disconnect
 sub disconnect {
     my $om = shift;
     return unless $om->{id};
-    $om->{async}->add(POST "$$om{server}/disconnect", [ id => $om->{id} ])
+    $om->post('disconnect');
+    delete $om->{id};
 }
+
+# http async post with id
+sub post {
+    my ($om, $event, @opts) = (shift, shift, @{+shift || []});
+    $om->{async}->add(POST "http://$$om{server}/$event", [ id => $om->{id}, @opts ]);
+}
+
+1
